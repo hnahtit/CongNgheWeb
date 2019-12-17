5c44eaf42d59c94003c0958a854fe231223c71e1
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-12-17 13:46:23 +0700

    add

---
+++ WEB_ShoesStore/WEB_ShoesStore/Content/js/cartController.js
@@ -0,0 +1,24 @@
+﻿var cart = {
+    init: function () {
+        cart.regEvent();
+    },
+    regEvent: function () {
+        $('#btnContinue').off('click').on('click', function () {
+            window.location.href = "/ ";
+        });
+        
+        /*$('#btnUpdate').off('click').on('click', function () {
+            var listProduct = $('.txtQuantity');
+            var cartList = []
+            $.each(listProduct, function (i, item) {
+                cartList.push({
+                    Quantity: $(item).val(),
+                    Product: {
+                        
+                    }
+                });
+            };
+        });*/
+    };
+}
+cart.init();
---
+++ WEB_ShoesStore/WEB_ShoesStore/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB_ShoesStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Shop()
+        {
+            return View();
+        }
+        public ActionResult Single(string id)
+        {   /*
+            var model = new F_Product().FindEntity(id);
+            return View(model);
+            */
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Search(string s)
+        {
+            /*
+            var model = new F_Product().DS_Product.Where(x => x.Product_Name.Contains(s)).ToList();
+            ViewBag.SP = model;
+            return View("Index", model);
+            */
+            return View();
+        }
+    }
+}
